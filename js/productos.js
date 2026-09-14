6ab808c1ff9c4ad593c963949068fdebb41388dc
Dylan <Dy.misetich@duocuc.cl>
2026-09-14 16:18:46 -0300

    Añade 11 productos al js y modularizacion del css

--- js/productos.js
+++ js/productos.js
@@ -31,5 +31,93 @@ const productos = [
         precio: 44990,
         imagen: "img/Elden Ring.webp",
         descripcion: "Un vasto mundo abierto lleno de peligros y misterios por descubrir."
+    },
+    {
+        id: 5,
+        nombre: "2k27",
+        plataforma: "PC / Xbox Series X",
+        precio: 29990,
+        imagen: "img/2k27.jpg",
+        descripcion: "NBA 2k27, la experiencia de simulacion deportiva de baloncesto mas completa"
+    },
+    {
+        id: 6,
+        nombre: "Borderland 3",
+        plataforma: "PC / Xbox Series X / PlayStation 5",
+        precio: 25990,
+        imagen: "img/Borderland3.jpg",
+        descripcion: "Clasico de Gearbox, explora pandora y consigue acceso a las bóvedas, sobrevive, combate y explora el mundo de Gehenna."
+    },
+    {
+        id: 7,
+        nombre: "Cyberpunk 2077",
+        plataforma: "PC / Xbox Series X / PlayStation 5",
+        precio: 39990,
+        imagen: "img/Cyberpunk_2077.webp",
+        descripcion: "Cyberpunk 2077 es un RPG de acción y mundo abierto donde modificas tu cuerpo con implantes cibernéticos para sobrevivir en la peligrosa metrópolis de Night City."
+    },
+    {
+        id: 8,
+        nombre: "DragonBall Fighters Z",
+        plataforma: "PlayStation 4 / Xbox One / PC / Nintendo Switch",
+        precio: 20990,
+        imagen: "img/dbfz.avif",
+        descripcion: "Dragon Ball FighterZ es un espectacular juego de peleas en 2.5D que revive los combates más icónicos del anime con gráficos y combates devastadores."
+    },
+    {
+        id: 9,
+        nombre: "Forza Horizon 6",
+        plataforma: "Xbox Series X / PC",
+        precio: 49990,
+        imagen: "img/fh6.avif",
+        descripcion: "Un espectacular juego de carreras en mundo abierto ambientado en Japón, donde exploras Tokio y compites con más de 550 autos"
+    },
+    {
+        id: 10,
+        nombre: "Half-Life 3",
+        plataforma: "PC",
+        precio: 99999,
+        imagen: "img/half-life-3-pc.jpg",
+        descripcion: "HL3 Ahora es personal (decido creer)"
+    },
+    {
+        id: 11,
+        nombre: "Project Zomboid",
+        plataforma: "PC",
+        precio: 7990,
+        imagen: "img/projectZomboid.jpg",
+        descripcion: "roject Zomboid es el simulador de supervivencia zombie definitivo: crea tu historia, gestiona recursos y enfréntate a una muerte inevitable en un mundo realista."
+    },
+    {
+        id: 12,
+        nombre: "Resident Evil Requiem",
+        plataforma: "PS5 / Xbox Series / Switch 2 / PC",
+        precio: 59990,
+        imagen: "img/reRequiem.avif",
+        descripcion: "Resident Evil Requiem es un survival horror que entrelaza el terror clásico de la analista Grace Ashcroft con la acción intensa del agente Leon S. Kennedy."
+    },
+    {
+        id: 13,
+        nombre: "Silksong",
+        plataforma: "PlayStation 5 y 4 / Xbox Series X|S y One / Nintendo Switch / Switch 2 / PC",
+        precio: 11990,
+        imagen: "img/Silksong.avif",
+        descripcion: "Aclamado metroidvania donde encarnas a Hornet, una letal cazadora que debe explorar, luchar y ascender por el misterioso reino de Pharloom."
+    },
+    {
+        id: 14,
+        nombre: "Tekken 7",
+        plataforma: "PS4 / Xbox ONE / PC",
+        precio: 20990,
+        imagen: "img/tekken7.avif",
+        descripcion: "Épico cierre de la saga del clan Mishima, un juego de peleas en 3D con gráficos espectaculares y combates intensos y cinematográficos."
+    },
+    {
+        id: 15,
+        nombre: "The Binding of Isaac: Repentance",
+        plataforma: "PC",
+        precio: 4990,
+        imagen: "img/thebindingofisaacrepentance.jpg",
+        descripcion: "La legendaria saga del Master Chief regresa con su aventura más amplia."
     }
 ];
